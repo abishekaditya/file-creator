e718a3d0c5d6984750d7eaaf68d1fa935899f49c
Abishek Aditya <abishekaditya@outlook.com>
2018-04-21 23:44:26 +0530

    Changed the title for commands

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('fileCreator.createFile', () => {
+    let disposable = vscode.commands.registerCommand('extension.createFile', () => {
         // The code you place here will be executed every time your command is executed
         const current_editor = vscode.window.activeTextEditor;
         if (!current_editor) {
@@ -35,7 +35,7 @@ export function activate(context: vscode.ExtensionContext) {
         openFile();
     });
 
-    vscode.commands.registerCommand('fileCreator.openFile', () => openFile());
+    vscode.commands.registerCommand('extension.openFile', () => openFile());
 
     context.subscriptions.push(disposable);
 }
